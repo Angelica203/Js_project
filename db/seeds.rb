baa05a04d68db4ee1b2b3fb2e152bc5a44f98917
Angelica Cruz <63663449+Angelica203@users.noreply.github.com>
2022-02-25 10:59:58 -0500

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+smoothies = Smoothie.create(flavor: "Green Hulk", image: "https://images.pexels.com/photos/1346347/pexels-photo-1346347.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
+smoothies = Smoothie.create(flavor: "Captain Berry", image: "https://cdn.pixabay.com/photo/2020/01/09/04/21/smoothie-4751820_1280.jpg")
+smoothies = Smoothie.create(flavor: "Mango King", image: "https://cdn.pixabay.com/photo/2017/05/05/01/50/smoothie-2285722_1280.jpg")
+smoothies = Smoothie.create(flavor: "Coco Loco", image: "https://cdn.pixabay.com/photo/2016/08/05/16/40/coconut-1572595_1280.jpg")
+smoothies = Smoothie.create(flavor: "Protein myster", image: "https://cdn.pixabay.com/photo/2020/05/27/15/52/food-5227754_1280.jpg")
+smoothies = Smoothie.create(flavor: "Strawberry cake", image: "https://cdn.pixabay.com/photo/2020/01/15/11/19/drink-4767705_1280.jpg")
+
+reviews = Review.create(comment: "this smoothie is very Good")
+reviews = Review.create(comment: "The captain berry is my favorite.")
+reviews = Review.create(comment: "Mango king is my first favorite and Green hulk is my second.")
